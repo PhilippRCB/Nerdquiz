39b07b4846daf5a4fa12baa562dd1403fdef38f3
PhilippRCB <pfilibb@gmail.com>
2026-04-05 22:58:13 +0200

    Quiztabelle aufgeräumt und beantwortete Fragen anders dargestellt

--- nerdquiz/src/main/java/com/klarmeister/nerdquiz/REST/QuizTabelleService.java
+++ nerdquiz/src/main/java/com/klarmeister/nerdquiz/REST/QuizTabelleService.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.klarmeister.nerdquiz.FrageBoardController;
-import com.klarmeister.nerdquiz.TeamController;
 
 
 @Controller
@@ -14,23 +13,11 @@ public class QuizTabelleService {
 
     @Autowired
     private FrageBoardController frageBoard;
-    @Autowired
-    private TeamController teamController;
     
 
     @GetMapping("/quizTabelle")
     public String frageBoard(Model model)  {
         model.addAttribute("kategorien", frageBoard.getFrageBoard().kategorien());
         return "quizTabelle"; 
-    }
-
-
-    @GetMapping("/frage/{kategorie}/{frageNummer}")
-    public String frage(String kategorie, int frageNummer)  {return frageBoard.frage(kategorie, frageNummer); }
-
-    @GetMapping("/frage/{kategorie}/{frageNummer}/{teamName}/{beantwortet}")
-    public void setFrageBeantwortet(String kategorie, String teamName, boolean beantwortet)  {
-        teamController.addTeam(teamName);
-        // TODO: Frage Abarbeiten
     }
 }
